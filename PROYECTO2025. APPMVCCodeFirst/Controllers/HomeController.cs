59fbe6d7f213e4f232ffdd8183a4e43777bef65e
Daniel Lope´z <daniel2006503@outlook.com>
2025-03-08 20:54:12 -0300

    Agregar archivos de proyecto.

---
+++ PROYECTO2025. APPMVCCodeFirst/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using PROYECTO2025._APPMVCCodeFirst.Models;
+using System.Diagnostics;
+
+namespace PROYECTO2025._APPMVCCodeFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PROYECTO2025. APPMVCCodeFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROYECTO2025._APPMVCCodeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
