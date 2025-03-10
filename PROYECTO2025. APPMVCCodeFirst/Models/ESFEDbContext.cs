dd68facfb4b9a0bcb856342f4a21aa9e743b06b6
Daniel Lope´z <daniel2006503@outlook.com>
2025-03-09 22:41:34 -0300

    Creé y configure el DbContext

---
+++ PROYECTO2025. APPMVCCodeFirst/Models/ESFEDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace PROYECTO2025._APPMVCCodeFirst.Models
+{
+    public class ESFEDbContext: DbContext 
+    {
+        public ESFEDbContext(DbContextOptions<ESFEDbContext> options) : base(options) { }
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Alumno> Alumnos { get; set; }
+    }
+}
