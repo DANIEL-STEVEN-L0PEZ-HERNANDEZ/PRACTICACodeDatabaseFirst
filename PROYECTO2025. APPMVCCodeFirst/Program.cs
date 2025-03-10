71ad1497a603b3cde3150ce0c00425eb6eb65622
Daniel Lope´z <daniel2006503@outlook.com>
2025-03-09 22:51:59 -0300

    Se actualizo el archivo Program

--- PROYECTO2025. APPMVCCodeFirst/Program.cs
+++ PROYECTO2025. APPMVCCodeFirst/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using PROYECTO2025._APPMVCCodeFirst.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ESFEDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
+
 
 var app = builder.Build();
 
